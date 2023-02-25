4e1289812607a39c5b3297e245f09aab65edafdb
Gautam Kumarm <g2gautamsingh@gmail.com>
2023-02-25 22:27:06 +0530

    basic structure done

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to cointab");
+});
+
 app.listen(PORT, async () => {
   console.log(`Server is running on port ${PORT}`);
 });
